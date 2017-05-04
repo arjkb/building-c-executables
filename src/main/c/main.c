a310df2a69c9f6ef611600f10f7481135e5a04ce
Schalk W. Cronjé <schalk@gradle.com>
2017-05-04 21:22:19 +0200

    Remove `char** env` from function signature

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>  // <1>
 #include "greeting.h" // <2>
 
-int main(int argc, char** argv, char** env) {
+int main(int argc, char** argv) {
     printf( "%s\n", GREETING_STRING);
     return 0;
 }
